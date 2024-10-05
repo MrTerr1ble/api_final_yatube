8ac95d71520009eb818c8e96639e11326b074f23
Misha <misha012004@gmail.com>
2024-10-05 19:04:15 +0300

    0.1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,26 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    group = serializers.SlugRelatedField(slug_field='id',
+                                         queryset=Group.objects.all(),
+                                         allow_null=True,
+                                         required=False)
 
     class Meta:
-        fields = '__all__'
+        fields = 'id', 'author', 'text', 'pub_date', 'image', 'group'
         model = Post
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        post = Post.objects.create(**validated_data)
+        return post
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +29,48 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('id', 'author', 'text', 'created', 'post')
+        read_only_fields = ('author', 'post')
         model = Comment
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        comment = Comment.objects.create(**validated_data)
+        return comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message="Вы уже подписаны на этого пользователя!"
+            )
+        ]
+
+    def validate(self, data):
+        user = self.context['request'].user
+        if user == data['following']:
+            raise ValidationError("Нельзя подписаться на самого себя!")
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet)
+
+comment_router = NestedDefaultRouter(router, 'posts', lookup='post')
+comment_router.register('comments', CommentViewSet)
 
 urlpatterns = [
+    path('', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
+    path('', include(comment_router.urls))
 ]
